a8eab2221efc5cca399664fde275bb35bb1bfd36
AJ <aj@laatu.uk>
2016-02-14 10:38:08 +0000

    added support for rhel6

--- rhel7.sh
+++ rhel7.sh
@@ -12,8 +12,16 @@ if which puppet > /dev/null 2>&1; then
   exit 0
 fi
 
-# add Puppet Labs public yum repo.
-sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm > /dev/null
+# Puppet Labs public yum repo for rhel7
+repoURL="https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm"
+
+if grep -qi "release 6" /etc/redhat-release; then
+  # Puppet Labs public yum repo for rhel6
+  repoURL="https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm"
+fi
+
+# install Puppet Labs public yum repo
+sudo rpm -ivh ${repoURL} > /dev/null
 
 # install Puppet
 sudo yum install -y puppet-agent > /dev/null
